39cc2d947f92c5cd7f58b4fd6b5c3b61ceb5801c
qiushuitian <qiushuitian1111@126.com>
2018-09-25 16:41:36 +0800

    moidfy

--- DJKit/UIKit/Extensions/UIAlertController+DJAdd.swift
+++ DJKit/UIKit/Extensions/UIAlertController+DJAdd.swift
@@ -7,19 +7,27 @@
 
 import UIKit
 
-extension UIAlertController {
-    
-    public func dj_showNormal(okTitle:String = "确定",
-                              cancelTitle:String = "取消",
-                              okHandler:((UIAlertAction) -> Void)? = nil) -> Void {
-//        let okAction = UIAlertAction(title: okTitle, style: UIAlertActionStyle.default, handler: okHandler)
-//        self.addAction(okAction)
-//        let cancelAction = UIAlertAction(title: cancelTitle, style: UIAlertActionStyle.cancel, handler: nil)
-//        self.addAction(cancelAction)
-//
-//
-//        if let rootViewController = UIApplication.shared.windows.last!.rootViewController {
-//            rootViewController.present(self, animated: true)
-//        }
-    }
-}
+
+//extension UIAlertController {
+//    
+//    public func dj_test() -> Void{
+//        print("test in")
+//    }
+//    
+//    public func dj_showNormal(okTitle:String,
+//                              cancelTitle:String,
+//                              okHandler:((UIAlertAction) -> Void)) -> Void {
+//        
+//        print("dj_showNormal in")
+//        
+////        let okAction = UIAlertAction(title: okTitle, style: UIAlertActionStyle.default, handler: okHandler)
+////        self.addAction(okAction)
+////        let cancelAction = UIAlertAction(title: cancelTitle, style: UIAlertActionStyle.cancel, handler: nil)
+////        self.addAction(cancelAction)
+////
+////
+////        if let rootViewController = UIApplication.shared.windows.last!.rootViewController {
+////            rootViewController.present(self, animated: true)
+////        }
+//    }
+//}
